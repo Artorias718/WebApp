a4bb0ff5cd4881d2d4cce95bc78c7c2df1a30e3e
gunito20 <guilherme.alesso@edu.unito.it>
2022-05-02 21:35:37 +0200

    recupero la lista di stabilimenti da tramite chiamata rest dal backend

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+// import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from "./App";
 import Mappa from "./Mappa";
--- src/routes/stabilimenti.jsx
+++ src/routes/stabilimenti.jsx
@@ -5,6 +5,7 @@ import {
     useSearchParams,
     useLocation
   } from "react-router-dom";
+import axios from 'axios';
   
 export default function Stabilimenti() {
     let [searchParams, setSearchParams] = useSearchParams();
@@ -12,78 +13,76 @@ export default function Stabilimenti() {
     const [stabilimenti, setStabilimenti] = useState([]);
 
     useEffect(() => {
-        // const fetchData = async () => {
-        //   // setIsLoading(true);
-    
-        //   try {
-        //       const result = await axios('http://localhost:8080/api/v1/stabilimenti/' + stabilimentoID + '/lista_Posti');
-    
-        //       setSeat(result.data);
-        //       setSeatAvailable(result.data.filter(s => s.booked === false));
-        //       setSeatReserved([]);
-        //   } catch (error) {
-        //       console.log(error);
-        //       alert(error);
-        //   }
-          
-        //   // setIsLoading(false);
-        // };
-    
-        // fetchData();
-    
-        const setData = () => {
-          let stabilimentiList = [
-            {
-                "id": 1,
-                "name": "Bagni Liguria",
-                "rowQty": 3,
-                "columnQty": 4,
-                "address": "Viale Rimembranza, 38, 16039 Sestri Levante GE",
-                "phoneNumber": "0185 482131",
-                "spotsNumber": 12
-            },
-            {
-                "id": 2,
-                "name": "Bagni Castelletto",
-                "rowQty": 2,
-                "columnQty": 5,
-                "address": "Via Aurelia, 17024 Finale Ligure SV",
-                "phoneNumber": "019 600106",
-                "spotsNumber": 10
-            },
-            {
-                "id": 3,
-                "name": "Capo Torre Beach & Lounge",
-                "rowQty": 5,
-                "columnQty": 3,
-                "address": "Via Aurelia di Ponente, 1, 17015 Celle Ligure SV",
-                "phoneNumber": "019 221 6264",
-                "spotsNumber": 15
-            },
-            {
-                "id": 4,
-                "name": "Bagni Vittoria Beach",
-                "rowQty": 4,
-                "columnQty": 5,
-                "address": "Lungomare Augusto Migliorini, 17024 Finale Ligure SV",
-                "phoneNumber": "019 695583",
-                "spotsNumber": 20
-            },
-            {
-                "id": 5,
-                "name": "Bagni Al Saraceno",
-                "rowQty": 2,
-                "columnQty": 7,
-                "address": "Via del Capo, 2, 17024 Finale ligure SV",
-                "phoneNumber": "019 698 8187",
-                "spotsNumber": 14
-            }
-        ];
-          setStabilimenti(stabilimentiList);
-        };
-    
-        setData();
-      }, []);
+      const fetchData = async () => {
+        // setIsLoading(true);
+  
+        try {
+            const result = await axios('http://localhost:8080/api/v1/stabilimenti/');
+  
+            setStabilimenti(result.data);
+        } catch (error) {
+            console.log(error);
+            alert(error);
+        }
+        
+        // setIsLoading(false);
+      };
+  
+      fetchData();
+  
+      const setData = () => {
+        let stabilimentiList = [
+          {
+              "id": 1,
+              "name": "Bagni Liguria",
+              "rowQty": 3,
+              "columnQty": 4,
+              "address": "Viale Rimembranza, 38, 16039 Sestri Levante GE",
+              "phoneNumber": "0185 482131",
+              "spotsNumber": 12
+          },
+          {
+              "id": 2,
+              "name": "Bagni Castelletto",
+              "rowQty": 2,
+              "columnQty": 5,
+              "address": "Via Aurelia, 17024 Finale Ligure SV",
+              "phoneNumber": "019 600106",
+              "spotsNumber": 10
+          },
+          {
+              "id": 3,
+              "name": "Capo Torre Beach & Lounge",
+              "rowQty": 5,
+              "columnQty": 3,
+              "address": "Via Aurelia di Ponente, 1, 17015 Celle Ligure SV",
+              "phoneNumber": "019 221 6264",
+              "spotsNumber": 15
+          },
+          {
+              "id": 4,
+              "name": "Bagni Vittoria Beach",
+              "rowQty": 4,
+              "columnQty": 5,
+              "address": "Lungomare Augusto Migliorini, 17024 Finale Ligure SV",
+              "phoneNumber": "019 695583",
+              "spotsNumber": 20
+          },
+          {
+              "id": 5,
+              "name": "Bagni Al Saraceno",
+              "rowQty": 2,
+              "columnQty": 7,
+              "address": "Via del Capo, 2, 17024 Finale ligure SV",
+              "phoneNumber": "019 698 8187",
+              "spotsNumber": 14
+          }
+      ];
+        setStabilimenti(stabilimentiList);
+      };
+  
+      // setData();
+    }, []);
 
     return (
       <div style={{ display: "flex" }}>
