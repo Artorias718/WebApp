6b77268c33e9bb1c9dc3c711778c785ce738817c
gunito20 <guilherme.alesso@edu.unito.it>
2022-05-26 19:01:42 +0200

    inserito modal per post per new spot

--- src/routes/expenses.jsx
+++ src/routes/expenses.jsx
@@ -3,14 +3,17 @@ import axios from 'axios';
 
 export default function Expenses() {
   const [placeID, setPlaceID] = useState('');
+  const [stabilimentoId, setStabilimentoId] = useState('');
+  const [price, setPrice] = useState('');
+  
+  const url = 'http://localhost:8080/api/v1';
   
   function handleNewStabGooglePlaceId(e) {
-    
     console.log(placeID);
     setPlaceID('');
 
     let googlePlaceId = placeID
-    let url = 'http://localhost:8080/api/v1'
+    // let url = 'http://localhost:8080/api/v1'
     if (googlePlaceId !== '') {
       axios.post(url + '/stabilimenti/create/' + googlePlaceId)
         .then(function (response) {
@@ -21,17 +24,45 @@ export default function Expenses() {
         }); 
     }
   }
+
+  function handleNewSpot(e) {
+    e.preventDefault();
+    console.log(stabilimentoId + ' ' + price);
+
+    let stabID = parseInt(stabilimentoId);
+    let spotPrice = parseFloat(price);
+
+    if (stabID !== NaN && spotPrice !== NaN) {
+      axios.post(url + '/stabilimenti/' + stabID + '/create_spot', {
+        price: spotPrice
+      })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+
+    //setStabilimentoId('');
+    //setPrice('');
+  }
   
   return (
       <main style={{ padding: "1rem 0" }}>
         <h2>Gestione</h2>
-        {/* <!-- Button trigger modal --> */}
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-          Nuovo Stabilimento con Google Place ID
-        </button>
+        {/* <!-- Button trigger new stabilimento with google place id modal --> */}
+        <div class="d-grid gap-2 col-6 mx-auto">
+          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newStabGooglePlaceIdModal">
+            Nuovo Stabilimento con Google Place ID
+          </button>
+          {/* <!-- Button trigger new spot modal --> */}
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newSpotModal">New Spot</button>
 
-        {/* <!-- Modal --> */}
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        </div>
+
+        {/* <!-- New Stabilimento with google Place ID Modal --> */}
+        <div class="modal fade" id="newStabGooglePlaceIdModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
@@ -54,6 +85,36 @@ export default function Expenses() {
             </div>
           </div>
         </div>
+
+      <div class="modal fade" id="newSpotModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">New spot</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="mb-3">
+                  <label for="stabilimento-id" class="col-form-label">Stabilimento ID:</label>
+                  <input type="text" class="form-control" id="stabilimentoID" placeholder="1"
+                    value={stabilimentoId} onChange={e => setStabilimentoId(e.target.value)} />
+                </div>
+                <div class="mb-3">
+                  <label for="prezzo" class="col-form-label">Prezzo:</label>
+                  <input type="text" class="form-control" id="prezzo" placeholder="37.99"
+                    value={price} onChange={e => setPrice(e.target.value)} />
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary" onClick={handleNewSpot}>Send message</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
 
       </main>
     );
